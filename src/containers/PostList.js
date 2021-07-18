fa8c54e8fc6cba583dde3b83587366e799abe75a
danielsumah <danielsumah@gmail.com>
2021-07-18 23:15:37 +0100

    Post list frontend done

--- src/containers/PostList.js
+++ src/containers/PostList.js
@@ -1,12 +1,81 @@
 import React from 'react'
-import { Header } from 'semantic-ui-react'
+import { Header, Item } from 'semantic-ui-react'
+import { useState, useEffect } from 'react'
+import Loaders from '../components/Loader'
+import axios from 'axios'
+import Message from '../components/Message'
 
 const PostList = () => {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        fetchPosts();
+        // getRecipes();
+    }, [])
+
+
+    const fetchPosts = async () => {
+        setLoading(true);
+
+        try {
+
+            console.log("old --------")
+            console.log(posts)
+            const res = await axios.get('http://127.0.0.1:8000/api/posts/');
+            const data = res.data
+            console.log("fetched data below --------")
+            console.log(data)
+            console.log("new state below")
+            setPosts(data)
+
+
+
+
+            setLoading(false);
+        } catch (error) {
+            setError(error.message);
+            setLoading(false)
+        }
+    }
+
     return (
         <div>
             <Header>Post list</Header>
+
+            {loading && <Loaders />}
+            {error && <Message negative message={error} />}
+            <Item.Group>
+                {posts.map(post => (
+                    <div>
+                        <Item key={post.id}>
+                            <Item.Image size='small' src={post.thumbnail} />
+
+                            <Item.Content>
+                                <Item.Header as='a'>{post.title}</Item.Header>
+                                <Item.Description>
+                                    <p>
+                                        {post.content}
+                                    </p>
+                                </Item.Description>
+                            </Item.Content>
+                        </Item>
+                    </div>
+                ))}
+            </Item.Group>
+
         </div>
     )
 }
 
 export default PostList;
+
+
+
+
+
+
+
+
+
